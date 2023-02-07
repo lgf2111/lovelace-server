569aadd46543257f5b1f8f724dfbfe75c84ba420
lgf2111 <lgf2111@gmail.com>
2023-02-07 21:25:55 +0800

    update server

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from lovelace import app, socketio
 def index():
     return "I'm alive!"
 
-use_localhost = os.environ.get("IN_DOCKER", False) and os.environ.get("REPL_ID", False)
+use_localhost = os.environ.get("IN_DOCKER", False) # and os.environ.get("REPL_ID", False)
 
 if __name__ == "__main__":
     host = "127.0.0.1" if use_localhost else "0.0.0.0"
--- lovelace/__init__.py
+++ lovelace/__init__.py
@@ -81,6 +81,7 @@ socketio = SocketIO(
         "127.0.0.1",
         "10.0.2.2",
         "https://lovelace-server.onrender.com",
+        "https://lovelace-server.lgf2111.repl.co/",
     ],
 )
 
