7a027610f099fce491d886a1c066f860ad2a36d2
lgf2111 <lgf2111@gmail.com>
2023-02-07 23:20:53 +0800

    update

--- run.py
+++ run.py
@@ -7,12 +7,8 @@ def index():
     return "I'm alive!"
 
 
-use_localhost = os.environ.get(
-    "IN_DOCKER", False
-)  # and os.environ.get("REPL_ID", False)
-
 if __name__ == "__main__":
-    host = "127.0.0.1" if use_localhost else "0.0.0.0"
+    host = "127.0.0.1" if os.environ.get("IN_DOCKER", False) else "0.0.0.0"
     socketio.run(
         app,
         debug=False,
