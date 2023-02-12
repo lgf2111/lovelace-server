cb9378eaa0cb5631b26d28b1d579f7f7936d6eba
lgf2111 <lgf2111@gmail.com>
2023-02-13 04:31:58 +0800

    update server

--- lovelace/chat/routes.py
+++ lovelace/chat/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from lovelace import chat_logger as logger
 from lovelace import socketio
-from flask_socketio import join_room, emit, leave_room,send
+from flask_socketio import join_room, emit, leave_room, send
 from lovelace.account.utils import token_required
 import secrets
 from lovelace import (
@@ -11,7 +11,7 @@ from lovelace import (
     mongo_temp_write,
     mongo_temp_read,
     mongo_chat_write,
-    mongo_chat_request_read
+    mongo_chat_request_read,
 )
 
 
@@ -48,23 +48,40 @@ def join(_, message):
     user2 = message["user2"]
     chat_collection = mongo_chat_write.chat
     chat_request_collection = mongo_chat_request_read.account_details
-    pubkey = message["pubkey"]
-    request_list = chat_request_collection.chat_request.find_one({"email":user2})["request"]
+    pubkey = message.get("pubkey")
+    # pubkey = message["pubkey"]
+    request_list = chat_request_collection.chat_request.find_one({"email": user2})[
+        "request"
+    ]
     approved = False
     for target_user_dict in request_list:
         if target_user_dict["target"] == user2:
-          approved = target_user_dict["approved"]
-          print(approved)
-          break
+            approved = target_user_dict["approved"]
+            print(approved)
+            break
     if approved == True:
-        room = chat_collection.chat.find_one({"$and": [{"user1": user1}, {"user2": user2}]})
+        room = chat_collection.chat.find_one(
+            {"$and": [{"user1": user1}, {"user2": user2}]}
+        )
         if room == None:
             room = chat_collection.chat.insert_one(
-                {"user1": message["user1"], "user2": message["user2"], "total_user":1, "pubkey1":pubkey, "pubkey2":""}
+                {
+                    "user1": message["user1"],
+                    "user2": message["user2"],
+                    "total_user": 1,
+                    "pubkey1": pubkey,
+                    "pubkey2": "",
+                }
             )
         else:
-            chat_collection.chat.update_one({"$and": [{"user1": user1}, {"user2": user2}]}, {"$set": {"total_user":2}})
-            chat_collection.chat.update_one({"$and": [{"user1": user1}, {"user2": user2}]}, {"$set": {"pubkey2":pubkey}})
+            chat_collection.chat.update_one(
+                {"$and": [{"user1": user1}, {"user2": user2}]},
+                {"$set": {"total_user": 2}},
+            )
+            chat_collection.chat.update_one(
+                {"$and": [{"user1": user1}, {"user2": user2}]},
+                {"$set": {"pubkey2": pubkey}},
+            )
 
         # join room
         room_name = str(room["_id"])
@@ -72,11 +89,17 @@ def join(_, message):
         response = f"{user1} has entered room ({room_name})."
 
         # get number of users
-        user_count = chat_collection.chat.find_one({"$and": [{"user1": user1}, {"user2": user2}]})["total_user"]
+        user_count = chat_collection.chat.find_one(
+            {"$and": [{"user1": user1}, {"user2": user2}]}
+        )["total_user"]
         if user_count == 2:
-            pkey1 = chat_collection.chat.find_one({"$and": [{"user1": user1}, {"user2": user2}]})["pubkey1"]
-            pkey2 = chat_collection.chat.find_one({"$and": [{"user1": user1}, {"user2": user2}]})["pubkey2"]
-            emit("message", {"pubkey1": pkey1, "pubkey2":pkey2}, room_name=room_name)
+            pkey1 = chat_collection.chat.find_one(
+                {"$and": [{"user1": user1}, {"user2": user2}]}
+            )["pubkey1"]
+            pkey2 = chat_collection.chat.find_one(
+                {"$and": [{"user1": user1}, {"user2": user2}]}
+            )["pubkey2"]
+            emit("message", {"pubkey1": pkey1, "pubkey2": pkey2}, room_name=room_name)
 
         print(response)
 
@@ -84,6 +107,7 @@ def join(_, message):
         emit("message", {"response": response}, room_name=room_name)
     else:
         send("Receipient has not approved sender for chat")
+
 
 @socketio.on("sent", namespace="/chat")
 @token_required()
@@ -111,5 +135,5 @@ def leave(_, message):
     )["_id"]
     # leaving the room
     leave_room(room=room)
-    chat_collection.chat.delete_one({"_id":room})
+    chat_collection.chat.delete_one({"_id": room})
     emit("message", {"response": f"{user1} has left the room."}, room=room)
